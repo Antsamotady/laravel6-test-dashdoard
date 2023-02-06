4aca04cd1b263adc85fe65aa4d9827268de8eaa4
Antsamotady <hasintsilavina@gmail.com>
2023-02-06 18:00:03 +0300

    Contact form

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+// Formulaire de contact
+Route::get('/contact', 'ContactsController@create')->name('contact.create');
+Route::post('/contact', 'ContactsController@store')->name('contact.store');
+
 
 // Must be at the end 
 Route::fallback(function () {
