17fe681dc438ea5daac122ffcf35a5ee4abb098b
Antsamotady <hasintsilavina@gmail.com>
2023-02-10 22:49:40 +0300

    Login page

--- routes/web.php
+++ routes/web.php
@@ -2,25 +2,9 @@
 
 use Illuminate\Support\Facades\DB;
 
-Route::get('/', function () {
-  $visited = DB::select('select * from places where visited = ?', [1]); 
-  $togo = DB::select('select * from places where visited = ?', [0]);
-
-  return view('travel_list', ['visited' => $visited, 'togo' => $togo ] );
-});
-
-Route::get('/welcome', function() {
-  return view('welcome');
-});
-
-Route::get('/user/{name?}',function($name = 'Virat Gandhi'){
-  echo "Name: ".$name;
-  })->name('anarana');
-
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 // Formulaire de contact
 Route::get('/contact', 'ContactsController@create')->name('contact.create');
