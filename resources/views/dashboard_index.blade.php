3240b6cc009069c6d9331d62e723c276847ab001
Antsamotady <hasintsilavina@gmail.com>
2023-04-12 12:11:03 +0300

    Add bootstrap to sass stylesheet

--- resources/views/dashboard_index.blade.php
+++ resources/views/dashboard_index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>Dashboard</title>
     <!-- Bootstrap CSS -->
-    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
     <div class="container-fluid">
