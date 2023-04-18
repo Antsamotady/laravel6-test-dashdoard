820b404478f924fffd849714dc52458e94c91d96
Antsamotady <hasintsilavina@gmail.com>
2023-04-18 12:25:08 +0300

    Vertical menu and breadcrumb

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,45 @@
 </head>
 <body>
     <div class="container">
-        <div class="header">HEADER</div>
+        <div class="header">
+            <ul class="nav justify-content-end">
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">Active</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="hero">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="{{ route('dashboard.index') }}">Dashboard</a></li>
+                    @if(Route::is('dashboard.menu1'))
+                        <li class="breadcrumb-item active" aria-current="page">Menu1</li>
+                    @elseif(Route::is('dashboard.menu3'))
+                        <li class="breadcrumb-item active" aria-current="page">Menu3</li>
+                    @elseif(Route::is('dashboard.menu2') || Route::is('dashboard.menu2Submenu1') || Route::is('dashboard.menu2Submenu2'))
+                        <li class="breadcrumb-item"><a href="{{ route('dashboard.menu2') }}">Menu2</a></li>
+                        @if(Route::is('dashboard.menu2Submenu1'))
+                            <li class="breadcrumb-item active" aria-current="page">Submenu1</li>
+                        @elseif(Route::is('dashboard.menu2Submenu2'))
+                            <li class="breadcrumb-item active" aria-current="page">Submenu2</li>
+                        @endif
+                    @endif
+                </ol>
+            </nav>
+        </div>
+
         @yield('content')
+
         <div class="footer">FOOTER</div>
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
